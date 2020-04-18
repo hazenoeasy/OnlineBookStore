2f0cbe325ce452168e034104f3c8203b0797c808
ChanceyZhang <33301662+zhangshaos@users.noreply.github.com>
2020-04-18 12:46:59 +0800

    修改了数据库表

--- backend/model/shopping_cart.go
+++ backend/model/shopping_cart.go
@@ -2,7 +2,10 @@ package model
 
 // ShoppingCart 一条购物车记录
 type ShoppingCart struct {
-    UserId  int `gorm:"column:user_id;foreignkey:user_id;association_foreignkey:user_id;not null"`
-    BookId  int `gorm:"column:book_id;foreignkey:book_id;association_foreignkey:book_id;not null"`
-    Amount  int `gorm:"column:amount;not null"`
+    UserId      int     `gorm:"column:user_id;foreignkey:user_id;association_foreignkey:user_id;not null"`
+    BookId      int     `gorm:"column:book_id;foreignkey:book_id;association_foreignkey:book_id;not null"`
+    Name        string  `gorm:"column:name;type:varchar(128);not null"`
+    CoverUrl    string  `gorm:"column:cover_url;type:varchar(128)"`
+    Price       int     `gorm:"column:price;not null"`
+    Amount      int     `gorm:"column:amount;not null"`
 }
